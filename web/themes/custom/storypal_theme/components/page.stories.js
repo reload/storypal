fb9e396c10f8d1cb1104a7ddbc2c93ecddffad07
Benjamin Rasmussen <benjamin@reload.dk>
2022-09-06 23:02:12 +0200

    Setup a basic Drupal installation + export config.

---
+++ web/themes/custom/storypal_theme/components/page.stories.js
@@ -0,0 +1,22 @@
+import drupalAttribute from "drupal-attribute";
+import template from './page.twig';
+import { ArticleList } from "./article-list.stories";
+
+export default {
+  title: "Pages/Pages",
+  parameters: {
+    layout: "fullscreen",
+    chromatic: { viewports: [320, 768, 1200] },
+  },
+};
+
+export const article = () => (
+  template({
+    attributes: new drupalAttribute(),
+    header: [],
+    content: [
+      ArticleList,
+    ],
+    bundle: "article",
+  })
+);
